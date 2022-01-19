0a2f2f0cd1013f8f15791447b33406d0a5e6fe56
TolikBu <tolikelement@gmail.com>
2022-01-20 03:11:03 +0500

    feat clientSlider.js

--- src/modules/clientSlider.js
+++ src/modules/clientSlider.js
@@ -1,60 +1,26 @@
 const clientSlider = () => {
-  const sliderContainer = document.querySelector('.benefits-inner');
-  const sliderTrack = document.querySelector('.benefits-wrap');
-  const itemSlide = document.querySelectorAll('.benefits__item');
-  const btnPrev = document.querySelector('.benefits__arrow--left');
-  const btnNext = document.querySelector('.benefits__arrow--right');
+const slider = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  loop: true,
 
-  let position = 0;
-  const slideToShow = 3;
-  const slideToScroll = 1;
-  const itemWidth = sliderContainer.clientWidth / slideToShow;
-  const movePosition = slideToScroll * itemWidth;
+  navigation: {
+    nextEl: '.benefits__arrow--left',
+    prevEl: '.benefits__arrow--right',
+  },
 
-  itemSlide.forEach((item) => {
-    item.style.minWidth = `${itemWidth}px`;
-  });
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
 
-  btnPrev.addEventListener('click', () => {
-    position -= movePosition;
-
-    setPosition();
-  });
-
-  btnNext.addEventListener('click', () => {
-    position += movePosition;
-
-    setPosition();
-  });
-
-
-  const setPosition = () => { 
-    sliderTrack.style.transform = `translateX(${position}px)`;
-  };
-  set
-  
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  },
+});
 };
 
 export default clientSlider;
-
-// const slider = new Swiper('.swiper', {
-//   slidesPerView: 1,
-//   spaceBetween: 0,
-
-//   navigation: {
-//     nextEl: '.benefits__arrow--left',
-//     prevEl: '.benefits__arrow--right',
-//   },
-
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false,
-//   },
-
-//   breakpoints: {
-//     576: {
-//       slidesPerView: 1,
-//       spaceBetween: 20,
-//     },
-//   },
-// });
